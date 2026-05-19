eccf3d61e6c5a0651a4a1b6be17cf8fe699f8b35
Lucas Coussirat <lukascoussirat@gmail.com>
2026-05-18 21:43:19 -0300

    Feat: API e Banco de Dados finalizados

---
+++ BradShield.API/BradShieldContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BradShield.API
+{
+    public class BradShieldContext : DbContext
+    {
+        public BradShieldContext(DbContextOptions<BradShieldContext> options) : base(options) { }
+
+        public DbSet<NumeroSeguro> NumerosSeguros { get; set; }
+    }
+}
