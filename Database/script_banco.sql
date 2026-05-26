fb5374e6ed3237f91336a260ad27dd3e52520499
Lucas Coussirat <lukascoussirat@gmail.com>
2026-05-26 20:45:56 -0300

    chore: limpa cache e corrige caminhos no gitignore

--- Database/script_banco.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE NumerosSeguros (
-    ID INT IDENTITY(1,1) PRIMARY KEY,
-    NumeroTelefone VARCHAR(20) NOT NULL,
-    InstituicaoID INT NOT NULL
-);
-
-INSERT INTO NumerosSeguros (NumeroTelefone, InstituicaoID) 
-VALUES ('0800-591-2117', 1);
